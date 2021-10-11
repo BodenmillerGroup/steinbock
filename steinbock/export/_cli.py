a423e3828ba9f14cde06cde29c4367f110d88fe1
Jonas Windhager <jonas.windhager@uzh.ch>
2021-10-11 16:24:06 +0200

    Add histocat export command (closes #62)

--- steinbock/export/_cli.py
+++ steinbock/export/_cli.py
@@ -1,8 +1,10 @@
 import click
 import networkx as nx
+import numpy as np
 
 from fcswrite import write_fcs
 from pathlib import Path
+from tifffile import imwrite
 from xtiff import to_tiff
 
 from steinbock import io
@@ -48,7 +50,12 @@ def export_cmd_group():
 @check_steinbock_version
 def ome_cmd(img_dir, panel_file, ome_dir):
     panel = io.read_panel(panel_file)
-    channel_names = panel["name"].tolist()
+    channel_names = [
+        f"{channel_id}_{channel_name}"
+        for channel_id, channel_name in zip(
+            panel["channel"].values, panel["name"].values
+        )
+    ]
     Path(ome_dir).mkdir(exist_ok=True)
     for img_file in io.list_image_files(img_dir):
         img = io.read_image(img_file)
@@ -58,6 +65,75 @@ def ome_cmd(img_dir, panel_file, ome_dir):
         to_tiff(img, ome_file, channel_names=channel_names)
         click.echo(ome_file.name)
         del img
+
+
+@export_cmd_group.command(
+    name="histocat", help="Export images to histoCAT for MATLAB"
+)
+@click.option(
+    "--img",
+    "img_dir",
+    type=click.Path(exists=True, file_okay=False),
+    default="img",
+    show_default=True,
+    help="Path to the image directory",
+)
+@click.option(
+    "--mask",
+    "mask_dir",
+    type=click.Path(file_okay=False),
+    default="masks",
+    show_default=True,
+    help="Path to the mask directory",
+)
+@click.option(
+    "--panel",
+    "panel_file",
+    type=click.Path(exists=True, dir_okay=False),
+    default="panel.csv",
+    show_default=True,
+    help="Path to the panel file",
+)
+@click.option(
+    "-o",
+    "histocat_dir",
+    type=click.Path(file_okay=False),
+    default="histocat",
+    show_default=True,
+    help="Path to the histoCAT export directory",
+)
+@check_steinbock_version
+def histocat_cmd(img_dir, mask_dir, panel_file, histocat_dir):
+    panel = io.read_panel(panel_file)
+    channel_names = [
+        f"{channel_id}_{channel_name}"
+        for channel_id, channel_name in zip(
+            panel["channel"].values, panel["name"].values
+        )
+    ]
+    img_files = io.list_image_files(img_dir)
+    mask_files = None
+    if Path(mask_dir).exists():
+        mask_files = io.list_mask_files(mask_dir, base_files=img_files)
+    Path(histocat_dir).mkdir(exist_ok=True)
+    for i, img_file in enumerate(img_files):
+        img = io.read_image(img_file, ignore_dtype=True)
+        histocat_img_dir = Path(histocat_dir) / img_file.stem
+        histocat_img_dir.mkdir(exist_ok=True)
+        for channel_name, channel_img in zip(channel_names, img):
+            imwrite(
+                histocat_img_dir / f"{channel_name}.tiff",
+                data=io._to_dtype(channel_img, np.float32),
+            )
+        mask = None
+        if mask_files is not None:
+            mask = io.read_mask(mask_files[i])
+            imwrite(
+                histocat_img_dir / f"{img_file.stem}_mask.tiff",
+                data=io._to_dtype(mask, np.float32),
+            )
+        click.echo(img_file.name)
+        del img, mask
 
 
 @export_cmd_group.command(
