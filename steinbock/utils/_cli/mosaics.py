6bb1334393704f610a19b5d6765bf207c86160e3
Jonas Windhager <jonas.windhager@uzh.ch>
2022-01-06 17:22:46 +0100

    in-memory mosaics

--- steinbock/utils/_cli/mosaics.py
+++ steinbock/utils/_cli/mosaics.py
@@ -1,5 +1,4 @@
 import click
-import re
 import sys
 
 from os import PathLike
@@ -7,13 +6,11 @@ from pathlib import Path
 from skimage import measure
 from typing import List, Sequence, Union
 
-from steinbock import io
 from steinbock._cli.utils import OrderedClickGroup
 from steinbock.utils import mosaics
-from steinbock.utils.mosaics import TileInfo
 
 
-def _collect_img_files(
+def _collect_tiff_files(
     img_files_or_dirs: Sequence[Union[str, PathLike]]
 ) -> List[Path]:
     img_files = []
@@ -21,7 +18,7 @@ def _collect_img_files(
         if Path(img_file_or_dir).is_file():
             img_files.append(Path(img_file_or_dir))
         else:
-            img_files += io.list_image_files(img_file_or_dir)
+            img_files += sorted(Path(img_file_or_dir).rglob("*.tiff"))
     return img_files
 
 
@@ -49,16 +46,11 @@ def mosaics_cmd_group():
     help="Path to the tile output directory",
 )
 def tile_cmd(images, tile_size, tile_dir):
-    img_files = _collect_img_files(images)
+    img_files = _collect_tiff_files(images)
     Path(tile_dir).mkdir(exist_ok=True)
-    for img_file, tile_info, tile in mosaics.try_extract_tiles_from_disk(
-        img_files, tile_size
+    for tile_file, tile in mosaics.try_extract_tiles_from_disk_to_disk(
+        img_files, tile_dir, tile_size
     ):
-        tile_file = Path(tile_dir) / (
-            f"{tile_info.img_file_stem}_tx{tile_info.x}_ty{tile_info.y}"
-            f"_tw{tile_info.width}_th{tile_info.height}.tiff"
-        )
-        io.write_image(tile, tile_file, ignore_dtype=True)
         click.echo(tile_file)
         del tile
 
@@ -79,46 +71,11 @@ def tile_cmd(images, tile_size, tile_dir):
     show_default=True,
     help="Relabel objects",
 )
-@click.option(
-    "--mmap/--no-mmap",
-    "use_mmap",
-    default=False,
-    show_default=True,
-    help="Use memory mapping",
-)
-def stitch_cmd(tiles, img_dir, relabel, use_mmap):
-    tile_files = _collect_img_files(tiles)
+def stitch_cmd(tiles, img_dir, relabel):
+    tile_files = _collect_tiff_files(tiles)
     Path(img_dir).mkdir(exist_ok=True)
-    tile_file_stem_pattern = re.compile(
-        r"(?P<img_file_stem>.*)_tx(?P<x>\d*)_ty(?P<y>\d*)"
-        r"_tw(?P<width>\d*)_th(?P<height>\d*)"
-    )
-    img_file_stems = []
-    img_tile_files = {}
-    img_tile_infos = {}
-    for tile_file in tile_files:
-        m = tile_file_stem_pattern.fullmatch(tile_file.stem)
-        if m is None:
-            click.echo(
-                f"WARNING: Malformed file name, ignoring file {tile_file}",
-                file=sys.stderr,
-            )
-            continue
-        tile_info = TileInfo(
-            m.group("img_file_stem"),
-            int(m.group("x")),
-            int(m.group("y")),
-            int(m.group("width")),
-            int(m.group("height")),
-        )
-        if tile_info.img_file_stem not in img_file_stems:
-            img_file_stems.append(tile_info.img_file_stem)
-            img_tile_files[tile_info.img_file_stem] = []
-            img_tile_infos[tile_info.img_file_stem] = []
-        img_tile_files[tile_info.img_file_stem].append(tile_file)
-        img_tile_infos[tile_info.img_file_stem].append(tile_info)
-    for img_file_stem, img in mosaics.try_stitch_tiles_from_disk(
-        img_file_stems, img_tile_files, img_tile_infos, use_mmap=use_mmap
+    for img_file, img in mosaics.try_stitch_tiles_from_disk_to_disk(
+        tile_files, img_dir
     ):
         if relabel:
             if img.ndim != 2:
@@ -127,8 +84,7 @@ def stitch_cmd(tiles, img_dir, relabel, use_mmap):
                     file=sys.stderr,
                 )
                 continue
-            img = measure.label(img)
-        img_file = Path(img_dir) / f"{img_file_stem}.tiff"
-        io.write_image(img, img_file, ignore_dtype=True)
+            img[:] = measure.label(img)
+            img.flush()
         click.echo(img_file)
         del img
--- steinbock/utils/mosaics.py
+++ steinbock/utils/mosaics.py
@@ -1,106 +1,101 @@
 import logging
-import mmap
 import numpy as np
-import tempfile
+import re
 import tifffile
 
 from os import PathLike
 from pathlib import Path
-from typing import Generator, Mapping, NamedTuple, Sequence, Tuple, Union
+from typing import Dict, Generator, List, NamedTuple, Sequence, Tuple, Union
 
 
 _logger = logging.getLogger(__name__)
 
 
-class TileInfo(NamedTuple):
-    img_file_stem: str
-    x: int
-    y: int
-    width: int
-    height: int
-
-
-def try_extract_tiles_from_disk(
-    img_files: Sequence[Union[str, PathLike]], tile_size: int
-) -> Generator[Tuple[Path, TileInfo, np.ndarray], None, None]:
+def try_extract_tiles_from_disk_to_disk(
+    img_files: Sequence[Union[str, PathLike]],
+    tile_dir: Union[str, PathLike],
+    tile_size: int,
+) -> Generator[Tuple[Path, np.ndarray], None, None]:
     for img_file in img_files:
         try:
-            img = tifffile.imread(img_file)
-            img_reshaped = np.moveaxis(img, (-1, -2), (0, 1))
+            img = tifffile.memmap(img_file)
             for tile_x in range(0, img.shape[-1], tile_size):
                 for tile_y in range(0, img.shape[-2], tile_size):
-                    tile_reshaped = img_reshaped[
-                        tile_x : (tile_x + tile_size),
+                    tile = img[
+                        ...,
                         tile_y : (tile_y + tile_size),
+                        tile_x : (tile_x + tile_size),
                     ]
-                    tile = np.moveaxis(tile_reshaped, (0, 1), (-1, -2))
-                    tile_info = TileInfo(
-                        Path(img_file).stem,
-                        tile_x,
-                        tile_y,
-                        tile.shape[-1],
-                        tile.shape[-2],
+                    tile_file = Path(tile_dir) / (
+                        f"{Path(img_file).stem}_tx{tile_x}_ty{tile_y}"
+                        f"_tw{tile.shape[-1]}_th{tile.shape[-2]}.tiff"
                     )
-                    yield Path(img_file), tile_info, tile
-                    del tile, tile_reshaped
-            del img, img_reshaped
+                    tifffile.imwrite(tile_file, tile)
+                    yield tile_file, tile
+                    del tile
+            del img
         except:
             _logger.exception(f"Error extracting tiles: {img_file}")
 
 
-def try_stitch_tiles_from_disk(
-    img_file_stems: Sequence[str],
-    img_tile_files: Mapping[str, Sequence[Union[str, PathLike]]],
-    img_tile_infos: Mapping[str, Sequence[TileInfo]],
-    use_mmap: bool = False,
+def try_stitch_tiles_from_disk_to_disk(
+    tile_files: Sequence[Union[str, PathLike]],
+    img_dir: Union[str, PathLike],
 ) -> Generator[Tuple[str, np.ndarray], None, None]:
-    for img_file_stem in img_file_stems:
-        tile_files = img_tile_files[img_file_stem]
-        tile_infos = img_tile_infos[img_file_stem]
+    class TileInfo(NamedTuple):
+        tile_file: Path
+        x: int
+        y: int
+        width: int
+        height: int
+
+    tile_file_stem_pattern = re.compile(
+        r"(?P<img_file_stem>.*)_tx(?P<x>\d*)_ty(?P<y>\d*)"
+        r"_tw(?P<width>\d*)_th(?P<height>\d*)"
+    )
+    img_tile_infos: Dict[str, List[TileInfo]] = {}
+    for tile_file in tile_files:
+        m = tile_file_stem_pattern.fullmatch(tile_file.stem)
+        if m is None:
+            raise ValueError(f"Malformed tile file name: {tile_file}")
+        img_file_stem = m.group("img_file_stem")
+        tile_info = TileInfo(
+            Path(tile_file),
+            int(m.group("x")),
+            int(m.group("y")),
+            int(m.group("width")),
+            int(m.group("height")),
+        )
+        if img_file_stem not in img_tile_infos:
+            img_tile_infos[img_file_stem] = []
+        img_tile_infos[img_file_stem].append(tile_info)
+    for img_file_stem, tile_infos in img_tile_infos.items():
+        img_file = Path(img_dir) / f"{img_file_stem}.tiff"
         try:
-            first_tile = tifffile.imread(tile_files[0])
-            first_tile_reshaped = np.moveaxis(first_tile, (-1, -2), (0, 1))
-            img_reshaped_shape = (
-                max(ti.x + ti.width for ti in tile_infos),
-                max(ti.y + ti.height for ti in tile_infos),
-            ) + first_tile_reshaped.shape[2:]
-            if use_mmap:
-                buffer_size = (
-                    np.prod(img_reshaped_shape) * first_tile.dtype.itemsize
-                )
-                buffer_file = tempfile.TemporaryFile()
-                buffer_file.write(b"\x00" * (buffer_size + 1))
-                buffer = mmap.mmap(
-                    buffer_file.fileno(), buffer_size, access=mmap.ACCESS_WRITE
-                )
-                img_reshaped = np.ndarray(
-                    img_reshaped_shape, dtype=first_tile.dtype, buffer=buffer
-                )
-            else:
-                img_reshaped = np.empty(
-                    img_reshaped_shape, dtype=first_tile.dtype
-                )
-            img_reshaped.fill(0)
-            img_reshaped[
-                tile_infos[0].x : tile_infos[0].x + tile_infos[0].width,
+            first_tile = tifffile.imread(tile_infos[0].tile_file)
+            img = tifffile.memmap(
+                img_file,
+                shape=first_tile.shape[:-2]
+                + (
+                    max(ti.y + ti.height for ti in tile_infos),
+                    max(ti.x + ti.width for ti in tile_infos),
+                ),
+                dtype=first_tile.dtype,
+            )
+            img[
+                ...,
                 tile_infos[0].y : tile_infos[0].y + tile_infos[0].height,
-            ] = first_tile_reshaped
-            if use_mmap:
-                buffer.flush()
-            for tile_file, tile_info in zip(tile_files[1:], tile_infos[1:]):
-                tile = tifffile.imread(tile_file)
-                tile_reshaped = np.moveaxis(tile, (-1, -2), (0, 1))
-                img_reshaped[
-                    tile_info.x : tile_info.x + tile_info.width,
+                tile_infos[0].x : tile_infos[0].x + tile_infos[0].width,
+            ] = first_tile
+            img.flush()
+            for tile_info in tile_infos[1:]:
+                img[
+                    ...,
                     tile_info.y : tile_info.y + tile_info.height,
-                ] = tile_reshaped
-                if use_mmap:
-                    buffer.flush()
-            img = np.moveaxis(img_reshaped, (0, 1), (-1, -2))
-            yield img_file_stem, img
-            del img, img_reshaped
-            if use_mmap:
-                buffer_file.close()
-                del buffer, buffer_file
+                    tile_info.x : tile_info.x + tile_info.width,
+                ] = tifffile.imread(tile_info.tile_file)
+                img.flush()
+            yield img_file, img
+            del img
         except:
-            _logger.exception(f"Error stitching tiles: {img_file_stem}")
+            _logger.exception(f"Error stitching tiles: {img_file}")
