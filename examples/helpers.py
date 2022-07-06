fce2d6fe142878ff6d23b5036352ac2f8a8e4f50
Nicolas Damond <nicolas.damond@gmail.com>
2022-07-06 13:56:51 +0000

    put normalization functions in helpers

--- examples/helpers.py
+++ examples/helpers.py
@@ -57,3 +57,37 @@ def extract_metadata(
 
     image_info_data = pd.DataFrame.from_dict([image_info_row])
     return(image_info_data)
+
+# Min-Max normalization
+def norm_minmax(img: np.ndarray):
+    channel_mins = np.nanmin(img, axis=(1, 2))
+    channel_maxs = np.nanmax(img, axis=(1, 2))
+    channel_ranges = channel_maxs - channel_mins
+    img -= channel_mins[:, np.newaxis, np.newaxis]
+    img[channel_ranges > 0] /= channel_ranges[
+        channel_ranges > 0, np.newaxis, np.newaxis
+    ]
+    return(img)
+
+# Z-score normalization
+def norm_zscore(img: np.ndarray):
+    channel_means = np.nanmean(img, axis=(1, 2))
+    channel_stds = np.nanstd(img, axis=(1, 2))
+    img -= channel_means[:, np.newaxis, np.newaxis]
+    img[channel_stds > 0] /= channel_stds[
+        channel_stds > 0, np.newaxis, np.newaxis
+    ]
+    return(img)
+
+# Group channels for segmentation stacks generation
+def segstack_channels(
+    img: np.ndarray,
+    channel_groups: np.ndarray,
+    aggregate
+):    
+    img = np.stack([
+            aggregate(img[channel_groups == channel_group], axis=0)
+            for channel_group in np.unique(channel_groups)
+            if not np.isnan(channel_group)
+        ])
+    return(img)
