8df67ba1a9500010b9dd51a525bae05066e29a01
Jonas Windhager <jonas.windhager@uzh.ch>
2022-10-31 12:45:11 +0100

    changes for steinbock v0.15

--- examples/01_preprocessing_imc.py
+++ examples/01_preprocessing_imc.py
@@ -6,7 +6,7 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.14.0
+#       jupytext_version: 1.13.8
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
@@ -14,7 +14,6 @@
 # ---
 
 # %%
-import numpy as np
 import pandas as pd
 
 from pathlib import Path
@@ -26,11 +25,14 @@ from steinbock.preprocessing import imc
 # %% [markdown]
 # # IMC preprocessing pipeline
 #
-# The full pipeline consists of three separate notebooks that have to be run successively: `preprocessing` (current notebook), `segmentation`, and `measurement`.
+# The full pipeline consists of three separate notebooks that have to be run
+# successively: `preprocessing` (current notebook), `segmentation`, and `measurement`.
 #
-# Here, we will extract image data from IMC aquisitions located in the `raw` folder and generate `.tiff` image stacks.
+# Here, we will extract image data from IMC aquisitions located in the `raw` folder and
+# generate `.tiff` image stacks.
 #
-# Before running your own script please check the [steinbock documentation](https://bodenmillergroup.github.io/steinbock).
+# Before running your own script please check the
+# [steinbock documentation](https://bodenmillergroup.github.io/steinbock).
 
 # %% [markdown]
 # ## Installation
@@ -38,15 +40,19 @@ from steinbock.preprocessing import imc
 # ```
 # conda create -n steinbock python=3.8
 # conda activate steinbock
-# pip install --upgrade tensorflow==2.8.0
-# pip install --upgrade -r requirements_deepcell.txt
-# pip install --no-deps deepcell==0.12.0
-# pip install --upgrade -r requirements.txt
-# pip install --no-deps steinbock[all]
 # conda install -c conda-forge jupyterlab
+# pip install --upgrade -r requirements_deepcell.txt
+# pip install --no-deps deepcell==0.12.3
+# pip install --upgrade -r requirements.txt
+# pip install --no-deps "steinbock[all]"
 # ```
-# The [requirements.txt](https://github.com/BodenmillerGroup/steinbock/blob/main/requirements.txt) and [requirements_deepcell.txt](https://github.com/BodenmillerGroup/steinbock/blob/main/requirements_deepcell.txt) files are located in the root folder of the `steinbock` repository.  
-# Up-to-date compatible package versions can be found here ("Package version conflicts"): https://bodenmillergroup.github.io/steinbock/latest/install-python/.
+# The
+# [requirements.txt](https://github.com/BodenmillerGroup/steinbock/blob/main/requirements.txt)
+# and
+# [requirements_deepcell.txt](https://github.com/BodenmillerGroup/steinbock/blob/main/requirements_deepcell.txt)
+# files are located in the root folder of the `steinbock` repository. Up-to-date
+# compatible package versions can be found here ("Package version conflicts"):
+# https://bodenmillergroup.github.io/steinbock/latest/install-python/.
 
 # %% [markdown]
 # ## Settings
@@ -57,16 +63,16 @@ from steinbock.preprocessing import imc
 # Folder structure:
 
 # %% [raw]
-# steinbock data/working directory  
-# ├── raw   
-# |    └──── *.zip (raw data)
-# ├── panel.csv (user-provided, when starting from raw data) 
-# ├── img (created by this script)  
-# ├── segstacks (created by this script)  
-# ├── masks (created by this script)  
-# ├── intensities (created by this script)  
-# ├── regionprops (created by this script)  
-# └── neighbors (created by this script)  
+# steinbock data/working directory
+# ├── raw
+# |    └── *.zip (raw data)
+# ├── panel.csv (user-provided, when starting from raw data)
+# ├── img (created by this script)
+# ├── segstacks (created by this script)
+# ├── masks (created by this script)
+# ├── intensities (created by this script)
+# ├── regionprops (created by this script)
+# └── neighbors (created by this script)
 
 # %%
 working_dir = Path(".")
@@ -83,50 +89,39 @@ img_dir.mkdir(exist_ok=True)
 
 # %% [markdown]
 # ## Download IMC example data
-# This section downloads IMC example raw data and the associated antibody panel file used for the experiment. The IMC raw data will be stored in the `raw` folder and the panel will be stored in the working directory.  
-# You can skip the following cell if you are processing your own data.
+# This section downloads IMC example raw data and the associated antibody panel file
+# used for the experiment. The IMC raw data will be stored in the `raw` folder and the
+# panel will be stored in the working directory. You can skip the following cell if you
+# are processing your own data.
 
 # %%
 for example_file_name, example_file_url in [
-    ("Patient1.zip", "https://zenodo.org/record/5949116/files/Patient1.zip", ),
-    ("Patient2.zip", "https://zenodo.org/record/5949116/files/Patient2.zip", ),
-    ("Patient3.zip", "https://zenodo.org/record/5949116/files/Patient3.zip", ),
-    ("Patient4.zip", "https://zenodo.org/record/5949116/files/Patient4.zip", ),
+    ("Patient1.zip", "https://zenodo.org/record/5949116/files/Patient1.zip"),
+    ("Patient2.zip", "https://zenodo.org/record/5949116/files/Patient2.zip"),
+    ("Patient3.zip", "https://zenodo.org/record/5949116/files/Patient3.zip"),
+    ("Patient4.zip", "https://zenodo.org/record/5949116/files/Patient4.zip"),
 ]:
     example_file = raw_dir / example_file_name
     if not example_file.exists():
         request.urlretrieve(example_file_url, example_file)
-        
+
 panel_file = working_dir / "panel.csv"
 if not panel_file.exists():
-    request.urlretrieve("https://zenodo.org/record/6642699/files/panel.csv",
-                        panel_file)
+    request.urlretrieve("https://zenodo.org/record/6642699/files/panel.csv", panel_file)
 
-# %% [markdown]
-# ## Unzip raw data files
-#
-# Documentation: https://bodenmillergroup.github.io/steinbock/latest/cli/preprocessing/#external-images
-#
-# zip folders should contain one `.mcd` file and all the associated `.txt` files.
-
-# %%
-# Extract .mcd files
-imc._extract_zips(path=raw_dir, suffix=".mcd", dest=raw_dir)
-
-# %%
-# Extract .txt files
-imc._extract_zips(path=raw_dir, suffix=".txt", dest=raw_dir)
 
 # %% [markdown]
 # ## Antibody panel
 #
 # ### Import the panel
-# The antibody panel file should be placed in the working directory (`examples` folder by default) and meet the steinbock format: https://bodenmillergroup.github.io/steinbock/latest/file-types/#panel.  
+# The antibody panel file should be placed in the working directory
+# (`examples` folder by default) and meet the steinbock format:
+# https://bodenmillergroup.github.io/steinbock/latest/file-types/#panel
 #
 # Customized panels should contain the following columns:
 # + `channel`: unique channel id, typically metal and isotope mass (e.g. `Ir191`)
-# + `name`: unique channel name.
-# + `deepcell`: channels to use for segmentation (1=nuclear, 2=membrane, empty/NaN=ignored).
+# + `name`: unique channel name
+# + `deepcell`: channels to use for segmentation (1=nuclear, 2=membrane, empty=ignored)
 # + `keep`: *(optional)* 1 for channels to preprocess, 0 for channels to ignore
 
 # %%
@@ -141,9 +136,9 @@ panel.head()
 # Alternatively, the panel can directly be created from `.mcd` files.
 #
 # In this case, the panel should be modified as following:
-# - Define which channels to retain for downstream processing (modify the `keep` column).
-# - Define channels to use for deep cell segmentation (see below).
-# - Additional modifications can be made (e.g., rename channel names) if needed.
+# - Define which channels to retain for downstream processing (modify the `keep` column)
+# - Define channels to use for deep cell segmentation (see below)
+# - Additional modifications (e.g., renaming channels) can be made if needed
 
 # %%
 create_panel_from_mcd = False
@@ -151,10 +146,13 @@ create_panel_from_mcd = False
 # %%
 if create_panel_from_mcd:
     # Create the panel
-    panel = imc.create_panel_from_mcd_files(imc.list_mcd_files(raw_dir))
+    mcd_files = imc.list_mcd_files(raw_dir, unzip=True)
+    panel = imc.create_panel_from_mcd_files(mcd_files, unzip=True)
 
-    # Channels that will not be keeped
-    channels_to_discard = ["ArAr80", "Xe131", "Xe134", "Ba136", "La138", "Pt196", "Pb206"]
+    # Channels that will not be kept
+    channels_to_discard = [
+        "ArAr80", "Xe131", "Xe134", "Ba136", "La138", "Pt196", "Pb206"
+    ]
     panel.loc[panel["channel"].isin(channels_to_discard), "keep"] = False
 
     # Channels for cell segmentation
@@ -162,7 +160,7 @@ if create_panel_from_mcd:
     membrane_channels = ["Sm147", "Sm149", "Sm152", "Ho165", "Yb173"]
     panel.loc[panel["channel"].isin(nuclear_channels), "deepcell"] = 1
     panel.loc[panel["channel"].isin(membrane_channels), "deepcell"] = 2
-    
+
     # Subset channels
     panel = panel[panel["keep"] == 1]
     print(panel.head())
@@ -176,7 +174,8 @@ io.write_panel(panel, panel_file)
 # %% [markdown]
 # ## Convert to images to tiff
 #
-# Documentation: https://bodenmillergroup.github.io/steinbock/latest/cli/preprocessing/#image-conversion
+# Documentation:
+# https://bodenmillergroup.github.io/steinbock/latest/cli/preprocessing/#image-conversion
 #
 # ### Settings
 # Image stacks are extracted from the acquisitions in `.tiff` format.
@@ -189,26 +188,32 @@ hpf = 50
 
 # %% [markdown]
 # ### Image conversion
-# Extract image stacks from IMC acquisitions (stored in the `img` subfolder) and export metadata as `images.csv`.
+# Extract image stacks from IMC acquisitions (stored in the `img` subfolder) and export
+# metadata as `images.csv`.
 
 # %%
-image_info_data = pd.DataFrame()
+image_info = []
 
-for mcd_file, acquisition, img, matched_txt, recovered in imc.try_preprocess_images_from_disk(
-    mcd_files = imc.list_mcd_files(raw_dir),
-    txt_files = imc.list_txt_files(raw_dir),
-    hpf = hpf,
-    channel_names = panel["channel"]
+mcd_files = imc.list_mcd_files(raw_dir, unzip=True)
+txt_files = imc.list_txt_files(raw_dir, unzip=True)
+for (
+    mcd_file, acquisition, img, matched_txt, recovered
+) in imc.try_preprocess_images_from_disk(
+    mcd_files, txt_files, hpf=hpf, channel_names=panel["channel"], unzip=True
 ):
     img_file = Path(img_dir) / f"{mcd_file.stem}_{acquisition.description}.tiff"
     io.write_image(img, img_file)
 
     if extract_metadata :
-        image_info = imc.create_image_info(mcd_file, acquisition, img, matched_txt, recovered, img_file)
-        image_info_data = pd.concat([image_info_data, pd.DataFrame([image_info])])
-        
+        image_info_row = imc.create_image_info(
+            mcd_file, acquisition, img, matched_txt, recovered, img_file
+        )
+        image_info.append(image_info_row)
+
+image_info = pd.DataFrame(data=image_info)
+
 if extract_metadata:
-    image_info_data.to_csv(working_dir / "images.csv", index=False)
+    image_info.to_csv(working_dir / "images.csv", index=False)
 
 # %%
 # !conda list
